765b4bf64c3803310b70f18e89dfb4459750fd87
NSkyPare32 <nachoparejo95@gmail.com>
2026-06-24 22:34:19 -0300

    Fixed/Error generar usuarios solucionado

--- Marketplace.Api/Config/Program.cs
+++ Marketplace.Api/Config/Program.cs
@@ -139,7 +139,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.ConfigObject.AdditionalItems["persistAuthorization"] = true);
 }
 
 // Delivery middleware pipeline
